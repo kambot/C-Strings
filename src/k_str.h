2cb5fe096544bb2bc84051e94c45ba2d24e09e5e
Kameron Penn <kameronmpenn@gmail.com>
2019-06-04 14:59:00 -0400

    first commit

---
+++ src/k_str.h
@@ -0,0 +1,255 @@
+#pragma once
+
+//get the length of a string
+int k_strlen(char *str) {
+    int len = 0;
+    while (*str != '\0') {
+        len++;
+        str++;
+    }
+    return len;
+}
+
+//return the uppercase character
+char k_chartoupper(char character) {
+    int a = character;
+    char charupper = a;
+    if (a >= 97 && a <= 172) {
+        charupper = a - 32;
+    }
+    return charupper;
+}
+
+//force a string to uppercase
+void k_toupper(char *str, char *outstr) {
+    int i = 0;
+    for (; *str != '\0'; str++) {
+        outstr[i++] = k_chartoupper(*str);
+    }
+    outstr[i] = '\0';
+}
+
+
+//copy a string into another
+void k_strcopy(char *str, char *outstr) {
+    int i = 0;
+    for (; *str != '\0'; str++) {
+        outstr[i++] = *str;
+    }
+    outstr[i] = '\0';
+}
+
+
+//compare two strings
+int k_strcmp(char *str, char *str1) {
+    for (; *str == *str1; ++str, ++str1) {
+        if (*str == '\0') {
+            return 0;
+        }
+    }
+    return *str - *str1;
+}
+
+//concatenate two strings
+void k_strappend(char *str, char *appendage, char *outstr) {
+    int i = 0;
+    for (; *str != '\0'; *str++)
+        outstr[i++] = *str;
+    for (; *appendage != '\0'; *appendage++)
+        outstr[i++] = *appendage;
+    outstr[i] = '\0';
+}
+
+//find and replace within a string
+void k_strrep(char *str, char *find, char *replace, char *outstr) {
+
+    int slen = k_strlen(str);
+    int flen = k_strlen(find);
+    int rlen = k_strlen(replace);
+
+    if (flen == 0)
+        return;
+
+    int newpos = 0;
+
+    for (int pos = 0; pos < slen; pos++) {
+
+        int match = 1;
+        if (str[pos] == find[0]) {
+
+            for (int posj = pos; posj < pos + flen; posj++) {
+                if (posj >= slen) {
+                    match = 0;
+                    break;
+                }
+                if (str[posj] == '\0') {
+                    match = 0;
+                    break;
+                }
+                if (str[posj] != find[posj - pos]) {
+                    match = 0;
+                    break;
+                }
+            }
+
+            if (match) {
+                for (int r = 0; r < rlen; r++) {
+                    outstr[newpos++] = replace[r];
+                }
+                pos += (flen - 1);
+                continue;
+            }
+
+        }
+
+        outstr[newpos++] = str[pos];
+    }
+    outstr[newpos] = '\0';
+}
+
+//returns the number of occurences of a substring in a string
+int k_strfind(char *str, char *find, int nested) {
+
+    int slen = k_strlen(str);
+    int flen = k_strlen(find);
+
+    if (flen == 0)
+        return 0;
+
+    int matches = 0;
+
+    for (int pos = 0; pos < slen; pos++) {
+
+        int match = 1;
+        if (str[pos] == find[0]) {
+
+            for (int posj = pos; posj < pos + flen; posj++) {
+                if (posj >= slen) {
+                    return matches;
+                }
+                if (str[posj] == '\0') {
+                    return matches;
+                }
+                if (str[posj] != find[posj - pos]) {
+                    match = 0;
+                    break;
+                }
+            }
+
+            if (match) {
+                matches++;
+                if (!nested)
+                    pos += (flen - 1);
+                continue;
+            }
+        }
+    }
+    return matches;
+}
+
+
+//returns the starting index of a substring in a string
+int k_strindex(char *str, char *find) {
+
+    int slen = k_strlen(str);
+    int flen = k_strlen(find);
+
+    if (flen == 0)
+        return -1;
+
+
+    for (int pos = 0; pos < slen; pos++) {
+
+        int match = 1;
+        if (str[pos] == find[0]) {
+
+            for (int posj = pos; posj < pos + flen; posj++) {
+                if (posj >= slen) {
+                    return -1;
+                }
+                if (str[posj] == '\0') {
+                    return -1;
+                }
+                if (str[posj] != find[posj - pos]) {
+                    match = 0;
+                    break;
+                }
+            }
+            if (match) {
+                return pos;
+            }
+        }
+    }
+    return -1;
+}
+
+//return a substring of a string
+char *k_substr(char *str, int start, int end) {
+
+    int slen = k_strlen(str);
+
+    int size = 0;
+    if (end >= start)
+        size = end - start;
+    else
+        size = start - end;
+    if (size < 0)
+        size *= -1;
+    size += 1;
+
+    char *substr = calloc(size, sizeof(char));
+
+    int v = end > start ? 1 : -1;
+
+    int pos = start;
+    int apos;
+    int spos = 0;
+    while (1) {
+        apos = pos;
+        if (pos >= slen) {
+            apos = pos % slen;
+        }
+        if (pos < 0) {
+            apos = pos % slen;
+            if (apos < 0)
+                apos += slen;
+        }
+
+        substr[spos++] = str[apos];
+
+        if (pos == end)
+            break;
+        pos += v;
+    }
+    substr[spos] = '\0';
+    return substr;
+}
+
+
+//string pad
+char *k_strpad(char *str, char pad, int length, int side) {
+
+    //side
+    //0 = left pad
+    //1 = right pad
+
+    int slen = k_strlen(str);
+    if (slen >= length)
+        return str;
+
+    char *padstr = calloc(length, sizeof(char));
+
+    int numpads = length - slen;
+    char *pads = calloc(numpads, sizeof(char));
+    for (int i = 0; i < numpads; i++)
+        pads[i] = pad;
+    pads[numpads] = '\0';
+
+    if (side != 1)
+        k_strappend(pads, str, padstr); //left side
+    else
+        k_strappend(str, pads, padstr); //right side
+
+    return padstr;
+
+}
---
+++ src/k_str_examples.c
@@ -0,0 +1,113 @@
+#include <stdio.h> 
+#include <stdlib.h>
+
+#include "k_str.h"
+
+
+
+
+void main() {
+
+    char* test_str = "This is a test.";
+    char* test2_str = "popqw~na;spn.qw97834=na";
+    char* test3_str = "000111001101";
+    char* numbers_str = "0123456789";
+
+    char* comp1_str = "abcd";
+    char* comp2_str = "abca";
+    char* comp3_str = calloc(4, sizeof(char));
+
+
+    //get the length of a string
+    printf("String length:\n");
+    int test_len = k_strlen(test_str);
+    printf("The sentence '%s', has %d characters.\n", test_str, test_len);
+
+    //convert to uppercase
+    printf("\nUppercase:\n");
+    char* test_upper = calloc(k_strlen(test2_str), sizeof(char));
+    k_toupper(test2_str, test_upper);
+    printf("'%s' --> '%s'\n", test2_str, test_upper);
+
+    //copy a string 
+    printf("\nCopy:\n");
+    printf("Before: '%s'\n", comp3_str);
+    k_strcopy(comp1_str, comp3_str);
+    printf("After: '%s'\n", comp3_str);
+
+    //compare strings
+    //comparison 1
+    printf("\nString comparison:\n");
+    int comp2 = k_strcmp(comp1_str, comp2_str);
+    if (comp2 != 0) {
+        printf("'%s' and '%s' are not equal.\n", comp1_str, comp2_str);
+    } else {
+        printf("'%s' and '%s' are equal.\n", comp1_str, comp2_str);
+    }
+    printf("Comparison value = %d\n", comp2);
+    //comparison 2
+    int comp3 = k_strcmp(comp1_str, comp3_str);
+    if (comp3 != 0) {
+        printf("'%s' and '%s' are not equal.\n", comp1_str, comp3_str);
+    }
+    else {
+        printf("'%s' and '%s' are equal.\n", comp1_str, comp3_str);
+    }
+    printf("Comparison value = %d\n", comp3);
+
+    //concatenate strings
+    printf("\nString concatenation:\n");
+    char* concat_str = calloc(k_strlen(numbers_str) + k_strlen(comp1_str), sizeof(char));
+    k_strappend(numbers_str, comp1_str, concat_str);
+    printf("'%s' + '%s' = '%s'\n", numbers_str, comp1_str, concat_str);
+
+    //find and replace
+    printf("\nFind and replace:\n");
+    char* rep_str = calloc(1000, sizeof(char));
+    k_strrep(test_str, " is ", " is not ", rep_str);
+    printf("String: '%s'\n", test_str);
+    printf("Find: '%s'\n", " is ");
+    printf("Replacement: '%s'\n", " is not ");
+    printf("Result: '%s'\n", rep_str);
+
+    //occurrences
+    printf("\nString occurrences:\n");
+    int o1 = k_strfind(test3_str, "00", 0);
+    int o2 = k_strfind(test3_str, "00", 1);
+    printf("The string '%s' occurrs %d times in '%s'\n", "00", o1, test3_str);
+    printf("The string '%s' occurrs %d times in '%s' (nested)\n", "00", o2, test3_str);
+
+
+    //starting index
+    printf("\nStarting index:\n");
+    int index = k_strindex(test2_str, "na");
+    int index2 = k_strindex(test2_str, "word");
+    printf("The starting index of 'na' in '%s' is %d.\n", test2_str, index);
+    printf("The starting index of 'word' in '%s' is %d.\n", test2_str, index2);
+
+    //substrings
+    printf("\nSubstrings:\n");
+    int nlen = k_strlen(numbers_str);
+    char* sub1 = k_substr(numbers_str, 0, 5);
+    char* sub2 = k_substr(numbers_str, -1, -2 * nlen);
+    char* sub3 = k_substr(numbers_str, 0, -5);
+    char* sub4 = k_substr(numbers_str, 0, 2 * nlen - 1);
+    printf("String: '%s'\n", numbers_str);
+    printf("Start = %d  End = %d   Result = '%s'\n", 0, 5, sub1);
+    printf("Start = %d End = %d Result = '%s'\n", -1, -2 * nlen, sub2);
+    printf("Start = %d  End = %d  Result = '%s'\n", 0, -5, sub3);
+    printf("Start = %d  End = %d  Result = '%s'\n", 0, 2 * nlen - 1, sub4);
+
+    //string padding
+    printf("\nString padding:\n");
+    char* pad1 = k_strpad(numbers_str, '*', 20, 1);
+    char* pad2 = k_strpad(numbers_str, '0', 15, 0);
+    char* pad3 = k_strpad(numbers_str, 'X', 10, 1);
+    printf("String: '%s'\n", numbers_str);
+    printf("pad 1: '%s'\n", pad1);
+    printf("pad 2: '%s'\n", pad2);
+    printf("pad 3: '%s'\n", pad3);
+
+
+    int m = 0;
+}
